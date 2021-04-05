00a7f52c3dfd06d5d4cf3f5c854a8bed0e1fab72
sreyansb <sreyansrocks@gmail.com>
2021-04-05 23:18:31 +0530

    FLushed Input

--- back-end/pythonfiles.py
+++ back-end/pythonfiles.py
@@ -10,14 +10,18 @@ p1 = Popen(['python3', '-m',"pdb",my_file], stdin=subprocess.PIPE, stdout=subpro
 flags = fcntl(p1.stdout, F_GETFL) # get current p.stdout flags
 #print(flags)
 fcntl(p1.stdout, F_SETFL, flags | O_NONBLOCK)
-p1.stdin.write("dir()\n".encode())
+sleep(1)
+# op=p1.communicate()[0].decode()
+# print(op)
 s=""
-sleep(0.2)#this time i checked some values from 0.5 to 0.1, 02 gave best results
+p1.stdin.write("dir()\n".encode())
+p1.stdin.flush()
+sleep(1)#this time i checked some values from 0.5 to 0.1, 02 gave best results
 while(True):
     try:
+        print("HERE")
         s+=read(p1.stdout.fileno(),1024).decode()
         #print("s",s)
     except Exception as E:
-        #print(E)
         break
 print(s)
