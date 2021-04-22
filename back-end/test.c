aa4b7f58a330179eb65cda8b251ba65876fccc73
Revanth <putturevanth@gmail.com>
2021-04-22 17:58:02 +0530

    some changes

--- back-end/test.c
+++ back-end/test.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 int g;
+extern void* resrsasr[1000];
+extern int resrsasr_i;
+
+struct node
+{
+	int a;
+	struct node *n;
+};
+
 
 int main()
 {
-	int *p;
-	p = (int *)malloc(sizeof(int));
-	*p = 100;
-	int a = 10;
+	struct node* a = (struct node*)malloc(sizeof(struct node));
+	struct node* b = (struct node*)malloc(sizeof(struct node));
+	//for(int i = 0; i < resrsasr_i; ++i)
+	//	printf("%p \n", resrsasr[i]);
+	a->n = b;
+	free(b);
+	a->n = NULL;
+	a->a = 123;
+	//for(int i = 0; i < resrsasr_i; ++i)
+	//	printf("%p \n", resrsasr[i]);
 }
