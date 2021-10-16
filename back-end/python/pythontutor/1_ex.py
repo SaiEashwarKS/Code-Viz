43309f11c27880b623561951e4270b10c1b5c580
sreyansb <sreyansrocks@gmail.com>
2021-10-16 23:53:54 +0530

    Final Changes for Graph

--- back-end/python/pythontutor/1_ex.py
+++ back-end/python/pythontutor/1_ex.py
@@ -1,4 +1,11 @@
-a = [1,2,3]
+class D:
+    def __init__(self,a,b) -> None:
+        self.a = a
+        self.b = b
+    def __repr__(self) -> str:
+        return "HELLO"
+f = D(5,6)
+a = [1,2,3,"saminamina",f]
 d = {"a":1, "b":2}
 
 S=6
@@ -9,3 +16,4 @@ if S==6:
     a.append(7)
 else:
     k=8
+#print(f)
